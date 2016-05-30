960cd1e58267f14f2799d1f7d0ca325253bc4e3f
gumao <598762549@qq.com>
2016-05-30 22:47:47 +0800

    Initial commit

---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by gumao
+ * Email: 598762549@qq.com
+ * Created Date:2016-05-30 22:46
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world!!!");
+    }
+}
